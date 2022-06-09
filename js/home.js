36ac23e9a067d56d999ae364248cbdfcbefcaf35
randyyz <ignasius_randy@yahoo.com>
2022-06-09 21:06:54 +0700

    main tinggal isi content

---
+++ js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.body-testimonial-content').slick({
+        prevArrow: '.prev',
+        nextArrow: '.next',
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+  });
